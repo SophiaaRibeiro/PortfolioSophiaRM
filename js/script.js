271da5623cddd02d7a4f23599c837606980788cc
Sophia Ribeiro M <126275606+SophiaaRibeiro@users.noreply.github.com>
2026-03-23 12:27:50 -0300

    att semanal

--- js/script.js
+++ js/script.js
@@ -21,3 +21,56 @@ observer.observe(card);
 });
 
 
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", ()=>{
+    menu.classList.toggle("active");
+});
+
+
+// const texto = "Sophia Ribeiro Mendonça";
+// let i = 0;
+
+// function digitar(){
+//     if(i < texto.length){
+//         document.getElementById("nome").innerHTML += texto.charAt(i);
+//         i++;
+//         setTimeout(digitar, 80);
+//     }
+// }
+
+// digitar();
+
+const palavras = [
+    "Sophia Ribeiro Mendonça",
+    "Desenvolvedora Front-End",
+];
+
+let i = 0;
+let j = 0;
+let apagando = false;
+
+function escrever(){
+
+    const elemento = document.getElementById("typing");
+
+    if(!apagando){
+        elemento.innerHTML = palavras[i].substring(0, j++);
+        if(j > palavras[i].length){
+            apagando = true;
+            setTimeout(escrever, 1500);
+            return;
+        }
+    }else{
+        elemento.innerHTML = palavras[i].substring(0, j--);
+        if(j === 0){
+            apagando = false;
+            i = (i + 1) % palavras.length;
+        }
+    }
+
+    setTimeout(escrever, apagando ? 50 : 100);
+}
+
+escrever();
